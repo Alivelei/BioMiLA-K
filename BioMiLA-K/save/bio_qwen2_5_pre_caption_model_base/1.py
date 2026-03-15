3a64f9098cf189c177f8f32d0e8f70313cb359a2
Alivelei <1968611917@qq.com>
2026-03-15 09:28:51 +0800

    first commit

--- BioMiLA-K/save/bio_qwen2_5_pre_caption_model_base/1.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-"""
-    @Time : 2026/1/24 16:40 
-    @Author : smile 笑
-    @File : 1.py
-    @desc :
-"""
--- BioMiLA-K/save/bio_qwen2_5_vqa_model/1.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-"""
-    @Time : 2026/1/24 16:38 
-    @Author : smile 笑
-    @File : 1.py
-    @desc :
-"""
